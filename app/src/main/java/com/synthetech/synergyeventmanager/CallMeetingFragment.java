a1310e221de9202116a9b1519acf15bebf458f88
synthetech-synergy <synthetech.io@gmail.com>
2017-02-08 22:02:15 +0530

    meeting

--- app/src/main/java/com/synthetech/synergyeventmanager/CallMeetingFragment.java
+++ app/src/main/java/com/synthetech/synergyeventmanager/CallMeetingFragment.java
@@ -6,6 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+
+import java.util.List;
 
 
 /**
@@ -25,10 +32,52 @@ public class CallMeetingFragment extends Fragment {
         // Inflate the layout for this fragment
         View callMeetingFragmentView = inflater.inflate(R.layout.fragment_call_meeting, container, false);
 
+        EditText topic_meeting = (EditText) callMeetingFragmentView.findViewById(R.id.topic_meeting);
+        EditText venue_meeting = (EditText) callMeetingFragmentView.findViewById(R.id.venue_meeting);
+        EditText time_meeting = (EditText) callMeetingFragmentView.findViewById(R.id.time_meeting);
+        EditText date_meeting = (EditText) callMeetingFragmentView.findViewById(R.id.date_meeting);
+
+        final RadioButton all_meeting = (RadioButton) callMeetingFragmentView.findViewById(R.id.all_meet);
+        final RadioButton dept_meeting = (RadioButton) callMeetingFragmentView.findViewById(R.id.dept_meet);
+
+        ListView all_members = (ListView) callMeetingFragmentView.findViewById(R.id.allMembersList);
+        final ListView dept_members = (ListView) callMeetingFragmentView.findViewById(R.id.deptList);
+
+        Button call_meeting = (Button) callMeetingFragmentView.findViewById(R.id.call_meeting);
+
+
+
+
+
+//--------------------------------------------------------------------------------------------------
+//--------------------------------------- RadioButtons ---------------------------------------------
+//--------------------------------------------------------------------------------------------------
+        all_meeting.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (all_meeting.isChecked()) {
+                    dept_members.setEnabled(false);
+                } else
+                    dept_meeting.setEnabled(true);
+            }
+        });
+        dept_meeting.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (dept_meeting.isChecked()) {
+                    all_meeting.setEnabled(false);
+                } else
+                    all_meeting.setEnabled(true);
+            }
+        });
+//--------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
 
 
 
         return callMeetingFragmentView;
     }
+
 
 }
